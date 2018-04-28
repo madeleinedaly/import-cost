c987156736a22a467397ba4b627086f718f55c2c
Madeleine Daly <mdaly@dailybreak.com>
2018-04-28 11:36:35 -0400

    add comment

--- import-cost.el
+++ import-cost.el
@@ -5,7 +5,7 @@
 ;; Author: Madeleine Daly <madeleine.faye.daly@gmail.com>
 ;; Maintainer: Madeleine Daly <madeleine.faye.daly@gmail.com>
 ;; Created: <2018-04-08 21:28:52>
-;; Last-Updated: <2018-04-28 11:26:08>
+;; Last-Updated: <2018-04-28 11:36:22>
 ;; Version: 1.0.0
 ;; Package-Requires: ((emacs "25.1") (epc "0.1.1") (ov "1.0.6"))
 ;; Keywords: javascript js
@@ -159,6 +159,7 @@ not already running."
   "Deactivates `import-cost-mode' in the current buffer.
 If no other buffers are actively using this minor mode, the EPC server will be stopped and unlinked."
   (when filename
+    ;; FIXME: TypeError: CreateListFromArrayLike called on non-object
     (epc:call-sync import-cost--epc-server 'disconnect (list filename))
     (import-cost--undecorate! filename))
   (when (and (null import-cost--decorations-list) import-cost--epc-server)
