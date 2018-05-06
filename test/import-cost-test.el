2e243ad2b63c458218ec47fc656933330786e91a
Madeleine Daly <mdaly@dailybreak.com>
2018-05-06 16:43:48 -0400

    fix tests, tweak travis config

--- test/import-cost-test.el
+++ test/import-cost-test.el
@@ -1,30 +1,27 @@
 ;;; import-cost-test.el --- Tests for import-cost.el
 
+;; Author: Madeleine Daly <madeleine.faye.daly@gmail.com>
+;; Last-Updated: <2018-05-06 16:43:28>
+
 (ert-deftest import-cost-lang/js ()
-  :tags '(utility languages)
   (should (string-equal import-cost--lang-javascript (import-cost--language "index.js"))))
 
 (ert-deftest import-cost-lang/jsx ()
-  :tags '(utility languages)
   (should (string-equal import-cost--lang-javascript (import-cost--language "index.jsx"))))
 
 (ert-deftest import-cost-lang/ts ()
-  :tags '(utility languages)
   (should (string-equal import-cost--lang-typescript (import-cost--language "index.ts"))))
 
 (ert-deftest import-cost-lang/tsx ()
-  :tags '(utility languages)
   (should (string-equal import-cost--lang-typescript (import-cost--language "index.tsx"))))
 
 (ert-deftest import-cost-ui/message ()
-  :tags '(utility ui)
   (let* ((import-cost-bundle-size-decoration 'both)
-         (package-info '((size . 203890) (size . 36890)))
+         (package-info '((size . 203890) (gzip . 36890)))
          (decoration-message (import-cost--get-decoration-message package-info)))
     (should (string-equal " 203KB (gzipped: 36KB)" decoration-message))))
 
 (ert-deftest import-cost-ui/color ()
-  :tags '(utility ui)
   (let* ((import-cost-large-package-color "#d44e40")
          (size (+ import-cost-medium-package-size (* 128 1024)))
          (package-info (list (cons 'size size)))
